10557084a1afc15b022fe563cb14d42d5c6e4cbc
takafumi-s <takafumi.s.dev@gmail.com>
2019-08-04 13:39:17 +0900

    version 1.0

---
+++ src/main/g8/modules/core/src/main/scala/com/example/core/Main.scala
@@ -0,0 +1,5 @@
+package com.example.core
+
+object Main extends App {
+  println("OK");
+}
---
+++ src/main/g8/project/Dependencies.scala
@@ -0,0 +1,12 @@
+import sbt._
+
+object Versions {
+  val Scala = "$scala_version$"
+}
+
+object Dependencies {
+  val coreDependencies = Seq()
+}
+
+object Libraries {
+}
